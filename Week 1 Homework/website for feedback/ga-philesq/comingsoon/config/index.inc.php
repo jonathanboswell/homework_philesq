deb2768298c492d97c98c6d53d419b8f74829731
philesq <LUWE@Louies-MacBook-Pro.local>
2016-09-20 20:02:40 -0700

    Week 1 Homework
    Adding week 1, portfolio folder, and javascript website for feedback.

---
+++ Week 1 Homework/website for feedback/ga-philesq/comingsoon/config/index.inc.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Index page configuration
+ *
+ * @package comingsoon
+ * @author jpeck@fluxsauce.com
+ *
+ *forked from github @lightcastercoldheart 
+ / alvarezmelvin@me.com
+ */
+
+global $_CONFIG;
+$_CONFIG = array();
+// All relative URLs will be converted to absolute URLs
+$_CONFIG['base_url'] = '/comingsoon/';
+// Style sheet, relative to css directory from base_url; required
+$_CONFIG['stylesheet'] = 'comingsoon.css';
+// Favorites icon, relative to images directory from base_url; optional, but highly recommended
+$_CONFIG['favicon'] = 'favicon.ico';
+// Page description; optional
+$_CONFIG['meta_description'] = 'This site has not yet launched.';
+// Page keywords, comma separated, no spaces; optional
+$_CONFIG['meta_keywords'] = 'philesq, louis alvarado';
+// Page title; required
+$_CONFIG['title'] = 'Philesq - coming soon...';
+// Photo; required
+// -- Filename, relative to images directory from base_url
+$_CONFIG['photo']['filename'] = 'soon.jpg';
+// -- Alt text
+$_CONFIG['photo']['alt'] = 'Philesq Soon';
+// -- Height in pixels
+$_CONFIG['photo']['height_px'] = 772;
+// -- Width in pixels
+$_CONFIG['photo']['width_px'] = 842;
+
+/**
+// Google Analytics Account ID; optional
+$_CONFIG['google_analytics_account_id'] = '';
+*/
+
+
+// Top caption; optional
+/**
+// -- Content
+$_CONFIG['caption_top']['content'] = '<h1>Coming soon...</h1>';
+// -- CSS Height, include units
+$_CONFIG['caption_top']['height'] = '100px';
+*/
+
+// Bottom caption; optional
+
+/**
+// -- Content
+$_CONFIG['caption_bottom']['content'] = '<h2>Stay tuned for details.</h2>';
+$_CONFIG['caption_bottom']['content'] .= '<div>This site has not yet launched.</div>';
+// -- CSS Height, include units
+$_CONFIG['caption_bottom']['height'] = '100px';
+*/
